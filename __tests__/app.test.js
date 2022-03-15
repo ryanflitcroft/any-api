85e80a2d6db689d735b263fb96dfa9d58210acd8
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-15 16:29:31 -0700

    Test written and passing for post/insert.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Album = require('../lib/models/Album');
 
 describe('any-api routes', () => {
   beforeEach(() => {
@@ -10,5 +11,27 @@ describe('any-api routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+
+  it('should be able to create an instance of Album', async () => {
+    const res = await request(app)
+      .post('/api/v1/albums')
+      .send({
+        title: 'So Sad So Sexy',
+        artist: 'Lykke Li',
+        year: 2018,
+        tracks: ['hard rain', 'deep end', 'two nights', 'Last Piece', 'Jaguars in the Air', 'sex money feelings die', 'So Sad So Sexy', 'Better Alone', 'Bad Woman', 'Utopia']
+      });
+    
+    expect(res.body).toEqual(
+      {
+        id: expect.any(String),
+        title: 'So Sad So Sexy',
+        artist: 'Lykke Li',
+        year: 2018,
+        tracks: ['hard rain', 'deep end', 'two nights', 'Last Piece', 'Jaguars in the Air', 'sex money feelings die', 'So Sad So Sexy', 'Better Alone', 'Bad Woman', 'Utopia']
+      }
+    );
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS albums
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-    id BIGINT GENERATED ALWAYS AS IDENTITY
-    title TEXT NOT NULL
-    artist TEXT NOT NULL
-    year SMALLINT
-    tracks text []
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    year SMALLINT,
+    tracks TEXT []
 );
